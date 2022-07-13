9526d2f5a1d89613396b8cb573638df43511fe03
ddeleon267 <dominique.a.deleon@gmail.com>
2022-07-13 12:40:31 -0400

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+import ipdb
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    sequence = []
+
+    while i < length:
+        # ipdb.set_trace()
+        if i == 0 or i == 1:
+            sequence.append(i)  
+        else: 
+            sequence.append(sequence[i - 1] + sequence[i - 2])
+        i += 1
+
+    for el in sequence:
+        print(el)
+# print_fibonacci(9)
